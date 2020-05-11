f301323680c109c7e254935daab5526236450423
Aaron Passchier <ap007@aquinas.edu>
2020-05-11 15:04:57 -0400

    Added CliOptions.cs

---
+++ Extractor/CliOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace Extractor
+{
+    public class CliOptions
+    {
+        [Option('d', "directory", Default = ".", HelpText = "The directory to start looking for .cat files.")]
+        public string Directory { get; set; }
+        [Option('o', "output", Default = "unpacked", HelpText = "The directory in which to store the extracted files.")]
+        public string OutputDirectory { get; set; }
+        [Option("cat-tool", Default = "XRCatTool", HelpText = "The location of the XRCatTool.exe. If you added the directory where you stored the file to path, you shouldn't need to provide this yourself.")]
+        public string CatToolPath { get; set; }
+    }
+}
