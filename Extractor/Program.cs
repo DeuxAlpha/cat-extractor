608d2b2c056ae8d26e2e018a800f44e9d374b1db
Aaron Passchier <ap007@aquinas.edu>
2020-05-11 15:54:23 -0400

    Updated app with comment if no files were found to extract

--- Extractor/Program.cs
+++ Extractor/Program.cs
@@ -14,6 +14,12 @@ namespace Extractor
 
                 var commands = extractor.BuildExtractionCommands().ToList();
 
+                if (!commands.Any())
+                {
+                    Console.WriteLine("No files were found.");
+                    return;
+                }
+
                 Console.WriteLine("The following commands will be executed if you proceed:");
                 foreach (var command in commands) Console.WriteLine(command.Command.Replace("/C ", ""));
                 Console.WriteLine("Proceed? Y/N");
