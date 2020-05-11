666d234f104f4a6cace9567141207fb899b72cd2
Aaron Passchier <ap007@aquinas.edu>
2020-05-11 15:49:01 -0400

    Added Executor option to CliOptions.cs

--- Extractor/CliOptions.cs
+++ Extractor/CliOptions.cs
@@ -10,5 +10,7 @@ namespace Extractor
         public string OutputDirectory { get; set; }
         [Option("cat-tool", Default = "XRCatTool", HelpText = "The location of the XRCatTool.exe. If you added the directory where you stored the file to path, you shouldn't need to provide this yourself.")]
         public string CatToolPath { get; set; }
+        [Option("executor", Default = "cmd.exe", HelpText = "The CLI to execute XRCatTool.exe. If not on Windows, you should adjust the default value.")]
+        public string Executor { get; set; }
     }
 }
